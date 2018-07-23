98b90b2e4988f0175974f9e39561f78f54a6a5b8
unknown <akshaykrishna016@gmail.com>
2018-07-23 18:23:41 +0530

    acc

---
+++ index.js
@@ -0,0 +1,11 @@
+$(document).ready(()=>{
+	$( "#btn1" ).click(function() {
+  $( ".bacon1" ).slideToggle( "slow" );
+});
+	$( "#btn2" ).click(function() {
+  $( ".bacon2" ).slideToggle( "slow" );
+});
+	$( "#btn3" ).click(function() {
+  $( ".bacon3" ).slideToggle( "slow" );
+});
+})
